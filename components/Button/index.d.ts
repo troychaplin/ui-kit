2109c4b0eb885fb335685f47e20e246c7c0388f3
troychaplin <troy.chaplin@gmail.com>
2024-02-24 16:30:01 +0000

    Deploying to gh-pages from @ troychaplin/ui-kit@9515c249d4375e2b8b34bd15aca957fdb8c43315 🚀

--- components/Button/index.d.ts
+++ components/Button/index.d.ts
@@ -1,7 +1,7 @@
 interface ButtonProps {
     primary?: boolean;
     backgroundColor?: string;
-    size?: "small" | "medium" | "large";
+    size?: 'small' | 'medium' | 'large';
     label: string;
     onClick?: () => void;
 }
