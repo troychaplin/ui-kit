3e4ce2a22d8e845bb7fa4125834a3765d7ce93ad
troychaplin <troy.chaplin@gmail.com>
2024-04-12 03:15:38 +0000

    Deploying to gh-pages from @ troychaplin/ui-kit@8975c03bd51e7ea235ae74ba089ccbc91d6ccbcc 🚀

---
+++ assets/WithTooltip-Y7J54OF7-BUNYYRJJ.js
@@ -0,0 +1,1 @@
+import{W as h,W as l,b as s}from"./index-Cuy9e1ma.js";import"./iframe-DryNtbni.js";import"../sb-preview/runtime.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";import"./index-B_J8iUie.js";import"./index-DLle_QMf.js";import"./index-DrFu-skq.js";export{h as WithToolTipState,l as WithTooltip,s as WithTooltipPure};
--- components/Button/index.d.ts
+++ components/Button/index.d.ts
@@ -1,9 +1,10 @@
 interface ButtonProps {
-    isOutline?: boolean;
     color?: string;
-    size?: 'sm' | 'md' | 'lg';
     label: string;
+    type?: 'button' | 'submit' | 'reset';
+    size?: 'sm' | 'md' | 'lg';
+    isOutline?: boolean;
     onClick?: () => void;
 }
-export declare const Button: ({ isOutline, size, color, label, ...props }: ButtonProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Button: ({ color, label, type, size, isOutline, ...props }: ButtonProps) => import("react/jsx-runtime").JSX.Element;
 export {};
