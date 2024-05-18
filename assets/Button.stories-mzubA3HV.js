2897a6cffbd4b5d8a0cc79f845151b766cdb0afb
troychaplin <troy.chaplin@gmail.com>
2024-05-18 02:41:48 +0000

    Deploying to gh-pages from @ troychaplin/ui-kit@06beb2d24b622a48ee10e1d123d288aab71d39f6 🚀

---
+++ assets/Button.stories-mzubA3HV.js
@@ -0,0 +1,6 @@
+import{B as e}from"./index-DfvUHPKU.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";const c={title:"Components/Button",component:e,tags:["autodocs"]},o={args:{label:"Button",bgColor:"#2b8164"}};var r,t,a;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    bgColor: '#2b8164'
+  }
+}`,...(a=(t=o.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const i=["Primary"];export{o as Primary,i as __namedExportsOrder,c as default};
