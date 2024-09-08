fb43b6b30341e37220473c4dfd195e3b4ca9a9bc
troychaplin <15524465+troychaplin@users.noreply.github.com>
2024-09-08 04:07:52 +0000

    Deploying to gh-pages from @ troychaplin/ui-kit@293d646b494195c8e9418c5599cb471e18173ff9 🚀

--- components/Card/index.d.ts
+++ components/Card/index.d.ts
@@ -1,12 +1,14 @@
-import { uiRoundedCorners, uiDropShadow } from '../../utils/tailwindProps';
+import { uiMaxWidth, uiRoundedCorners, uiDropShadow } from '../../utils/tailwindProps';
+type MaxWidthKeys = keyof typeof uiMaxWidth;
 type RoundedKeys = keyof typeof uiRoundedCorners;
 type ShadowKeys = keyof typeof uiDropShadow;
 export interface CardProps {
     children?: React.ReactNode;
+    maxWidth?: MaxWidthKeys;
     rounded?: RoundedKeys;
     shadow?: ShadowKeys;
     borderWidth?: number;
     borderColor?: string;
 }
-export declare const Card: ({ children, rounded, shadow, borderWidth, borderColor }: CardProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Card: ({ children, maxWidth, rounded, shadow, borderWidth, borderColor }: CardProps) => import("react/jsx-runtime").JSX.Element;
 export {};
--- components/CardGroup/index.d.ts
+++ components/CardGroup/index.d.ts
@@ -1,6 +1,9 @@
+import { uiGridColumns } from '../../utils/tailwindProps';
+type GridColumnsKeys = keyof typeof uiGridColumns;
 export interface CardGroupProps {
     children?: React.ReactNode;
-    cols?: 1 | 2 | 3 | 4;
+    cols?: GridColumnsKeys;
     gap?: number;
 }
 export declare const CardGroup: ({ children, cols, gap }: CardGroupProps) => import("react/jsx-runtime").JSX.Element;
+export {};
