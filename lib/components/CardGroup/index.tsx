96711278f6e0341672fe565922f1c130b981924c
Troy Chaplin <troy.chaplin@gmail.com>
2024-09-07 23:44:33 -0400

    change: card and card group props

--- lib/components/CardGroup/index.tsx
+++ lib/components/CardGroup/index.tsx
@@ -1,8 +1,10 @@
 import { uiGridColumns } from '../../utils/tailwindProps'
 
+type GridColumnsKeys = keyof typeof uiGridColumns
+
 export interface CardGroupProps {
   children?: React.ReactNode
-  cols?: 1 | 2 | 3 | 4
+  cols?: GridColumnsKeys
   gap?: number
 }
 
